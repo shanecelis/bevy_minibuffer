c61837ab8d95fb57c5bca2b1d881fde204e28437
Shane Celis <shane.celis@gmail.com>
2024-12-18 22:56:33 -0500

    feature: Add sink::result.

--- src/sink.rs
+++ src/sink.rs
@@ -6,6 +6,18 @@ use bevy_defer::{AsyncExecutor, NonSend};
 use std::{fmt::Display, future::Future};
 
 /// Show error if any in minibuffer.
+pub fn result<T, E>(In(result): In<Result<T,E>>,
+                    mut minibuffer: Minibuffer)
+where T: 'static,
+      E: 'static + Display
+{
+    if let Err(e) = result {
+        minibuffer.message(format!("{e}"));
+    }
+}
+
+// TODO: Rename these to no sink suffix. Use the sink module name.
+/// Show error if any in minibuffer.
 pub fn future_result_sink<
     T: 'static,
     E: 'static + Display,
